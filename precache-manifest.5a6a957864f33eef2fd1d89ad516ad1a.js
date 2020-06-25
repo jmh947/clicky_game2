da3582abfcd0a5c114dc3d0133ab1d9032852757
Jaclyn Hardy <jackihardy@yahoo.com>
2020-06-24 22:34:42 -0700

    Updates

---
+++ precache-manifest.5a6a957864f33eef2fd1d89ad516ad1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76f85582193dacf0fde5d2c59a69b99",
+    "url": "/clicky_game2/index.html"
+  },
+  {
+    "revision": "5b40819f06a69f6f15d4",
+    "url": "/clicky_game2/static/js/2.1fb5b027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky_game2/static/js/2.1fb5b027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f86ad5da7f69d0aec1",
+    "url": "/clicky_game2/static/js/main.fda96bf8.chunk.js"
+  },
+  {
+    "revision": "fb264f4df94006ca0fc9",
+    "url": "/clicky_game2/static/js/runtime-main.745d1166.js"
+  }
+]);
